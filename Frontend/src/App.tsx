3aa02af1c558018533756ce23d060cbba6036ded
JavierAChacon <alejandro.chr@outlook.es>
2024-03-02 15:51:02 -0400

    fix(routes): fix path '/'

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Layout from './components/Layout'
 
 function App (): JSX.Element {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/'>
+        <Route path='/' element={<Layout />}>
           <Route />
         </Route>
       </Routes>
