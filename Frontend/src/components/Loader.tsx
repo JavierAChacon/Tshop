508a24a2d976a6b80dcd2a697c67393fc975533d
JavierAChacon <alejandro.chr@outlook.es>
2024-03-08 22:07:51 -0400

    style(components): change width and height of Loader component

--- Frontend/src/components/Loader.tsx
+++ Frontend/src/components/Loader.tsx
@@ -1,9 +1,7 @@
 import { TbLoader3 } from 'react-icons/tb'
 
 const Spinner = (): JSX.Element => (
-  <div className='spin flex h-[calc(100dvh-2.5rem)] items-center justify-center'>
-    <TbLoader3 className='h-10 w-10 animate-spin' />
-  </div>
+  <TbLoader3 className='h-10 w-10 animate-spin md:h-20 md:w-20 lg:h-28 lg:w-28' />
 )
 
 export default Spinner
