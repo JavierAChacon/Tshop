59703065d96b609484ec56f4a561612cea56f326
JavierAChacon <alejandro.chr@outlook.es>
2024-03-27 18:35:50 -0400

    feat(pages): add Checkout Success page

--- Frontend/src/pages/CheckoutSuccess.tsx
+++ Frontend/src/pages/CheckoutSuccess.tsx
@@ -1,7 +1,20 @@
-const CheckoutSuccess = () => {
+import { CiCircleCheck } from 'react-icons/ci'
+import { Link } from 'react-router-dom'
+
+const CheckoutSuccess = (): JSX.Element => {
   return (
-    <div>
-      <h1>Checkout Success</h1>
+    <div className='flex h-[calc(100dvh-2.5rem)] flex-col items-center justify-center text-center'>
+      <div className='text-9xl text-green-500'>
+        <CiCircleCheck />
+      </div>
+      <p className='text-lg md:text-2xl'>
+        The order confirmation email with details of your order and a link to
+        track its progress has been sent to your email addres
+      </p>
+
+      <button className='my-5 rounded-xl bg-gray-700 p-2 text-lg text-white transition-all duration-300 hover:scale-105'>
+        <Link to='/'>Understood!</Link>
+      </button>
     </div>
   )
 }
